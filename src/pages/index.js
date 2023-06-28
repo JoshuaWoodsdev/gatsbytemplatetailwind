132b6001b449351ceb9a7314d193c9fee18feb48
Joshua Woods <nordev@protonmail.com>
2023-06-28 14:26:10 -0500

    detup

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import * as React from "react"
+
+const IndexPage = () => {
+  return (
+  
+    <h1 className="text-3xl font-bold p4">
+      Hello world!
+    </h1>
+
+  )
+}
+
+export default IndexPage
+
+export const Head = () => <title>Home Page</title>
